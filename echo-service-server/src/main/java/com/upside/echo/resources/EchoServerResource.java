a99f537a8389ce8c90e93f83adcae4ebc9af5282
Thomas Vaughan <tom@upside-services.com>
2017-09-21 19:50:31 +0200

    Adds an endpoint that fails 50% of the time
    Adds an endpoint that fails 50% of the time to facilitate the
    development of some backoff client wrapping changes we want to make

--- echo-service-server/src/main/java/com/upside/echo/resources/EchoServerResource.java
+++ echo-service-server/src/main/java/com/upside/echo/resources/EchoServerResource.java
@@ -1,11 +1,15 @@
 package com.upside.echo.resources;
 
 import java.util.Map;
+import java.util.Random;
 
 /**
  * Echo server fun times.
  */
 public class EchoServerResource implements EchoResource {
+
+    private final Random random = new Random();
+
     @Override
     public Map<String, String> getEnv() {
         return System.getenv();
@@ -14,5 +18,15 @@ public class EchoServerResource implements EchoResource {
     @Override
     public String getHello() {
         return "good night booty";
+    }
+
+    @Override
+    public String fail50() {
+        if (this.random.nextFloat() < 0.5f) {
+            throw new IllegalStateException("This is a randomly occuring error");
+        }
+        else {
+            return "hello";
+        }
     }
 }
